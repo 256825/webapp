29bf5ecd7b030980eb5a25213467362738cd8e4e
256825 <98037580+256825@users.noreply.github.com>
2022-11-22 13:28:11 -0600

    Nov 22

--- script.js
+++ script.js
@@ -28,3 +28,76 @@ function hide(){
 
 }
 
+
+//JACKIE'S NOTES
+if (localStorage["sunday"]) {
+    var sunday = localStorage["sunday"];
+    document.getElementById("sunday").value = sunday;
+  } 
+  function saveSunday() {
+    var sunday = document.getElementById("sunday").value;
+    localStorage.setItem("sunday", sunday);
+  }
+if (localStorage["monday"]) {
+    var monday = localStorage["monday"];
+    document.getElementById("monday").value = monday;
+  } 
+  function saveMonday() {
+    var monday = document.getElementById("monday").value;
+    localStorage.setItem("monday", monday);
+  }
+  
+  if (localStorage["tuesday"]) {
+    var tuesday = localStorage["tuesday"];
+    document.getElementById("tuesday").value = tuesday;
+  }
+  function saveTuesday() {
+    var tuesday = document.getElementById("tuesday").value;
+    localStorage.setItem("tuesday", tuesday);
+  }
+  
+  if (localStorage["wednesday"]) {
+    var wednesday = localStorage["wednesday"];
+    document.getElementById("wednesday").value = wednesday;
+  }
+  function saveWednesday() {
+    var wednesday = document.getElementById("wednesday").value;
+    localStorage.setItem("wednesday", wednesday);
+  }
+  
+  if (localStorage["thursday"]) {
+    var thursday = localStorage["thursday"];
+    document.getElementById("thursday").value = thursday;
+  }
+  function saveThursday() {
+    var thursday = document.getElementById("thursday").value;
+    localStorage.setItem("thursday", thursday);
+  }
+  
+  if (localStorage["friday"]) {
+    var friday = localStorage["friday"];
+    document.getElementById("friday").value = friday;
+  }
+  function saveFriday() {
+    var friday = document.getElementById("friday").value;
+    localStorage.setItem("friday", friday);
+  }
+  if (localStorage["saturday"]) {
+    var saturday = localStorage["saturday"];
+    document.getElementById("saturday").value = saturday;
+  } 
+  function saveSaturday() {
+    var saturday = document.getElementById("saturday").value;
+    localStorage.setItem("saturday", saturday);
+  }
+  
+  var show = false;
+  function showNote() {
+    if (show == false) {
+      document.getElementById("notes").style.opacity = '1';
+      show = true;
+    } else if (show == true) {
+      document.getElementById("notes").style.opacity = '0';
+      show = false;
+    }
+  }
